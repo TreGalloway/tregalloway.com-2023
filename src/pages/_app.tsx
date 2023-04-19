6a0937a97b98082c9168f82c2fbacb4b7c14c9dc
Tre <tre@Tres-MacBook-Air.local>
2023-04-19 17:36:36 -0500

    Adding vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import '@/styles/prism-plus.css';
 import 'nprogress/nprogress.css';
 import '@/styles/nprogress-custom.scss';
 
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import { appWithTranslation } from 'next-i18next';
@@ -82,6 +83,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
         <LayoutWrapper>
           <Component {...pageProps} />
+          <Analytics />
         </LayoutWrapper>
       </CommandPalette>
     </ThemeProvider>
